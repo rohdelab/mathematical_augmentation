1c1ae3b8ddc12a2c282aed25bd66cc3b7fcedd64
Mohammad Shifat E Rabbi <51829986+mr2kzUVa@users.noreply.github.com>
2021-11-19 09:24:03 -0500

    Add files via upload

--- code/cnn.sh
+++ code/cnn.sh
@@ -3,16 +3,17 @@
 # Use different models to do classification on different datasets
 
 
-python CNNc.py --dataset SYNTH --model shallowcnn --naug 100
-python CNNc.py --dataset SYNTH --model vgg11 --naug 1
-python CNNc.py --dataset SYNTH --model vgg11 --naug 10
-python CNNc.py --dataset MNIST --model vgg11 --naug 100
-python CNNc.py --dataset SYNTH --model vgg11 --naug 100
-python CNNc.py --dataset AFFNISTb_out --model shallowcnn --naug 100
-python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 1
-python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 10
-python CNNc.py --dataset AFFNISTb --model vgg11 --naug 100
-python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 100
-python CNNc.py --dataset OMNIGLOT --model resnet18 --naug 100
-python CNNc.py --dataset OMNIGLOT --model shallowcnn --naug 100
-python CNNc.py --dataset OMNIGLOT --model vgg11 --naug 100
+python CNNc.py --dataset MNIST --model vgg11 --naug 50
+python CNNc.py --dataset SYNTH --model vgg11 --naug 50
+python CNNc.py --dataset AFFNISTb --model vgg11 --naug 50
+python CNNc.py --dataset OMNIGLOT --model vgg11 --naug 50
+python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 50
+
+python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 25
+python CNNc.py --dataset SYNTH --model vgg11 --naug 25
+python CNNc.py --dataset OMNIGLOT --model vgg11 --naug 25
+python CNNc.py --dataset MNIST --model vgg11 --naug 25
+python CNNc.py --dataset AFFNISTb --model vgg11 --naug 25
+
+
+
