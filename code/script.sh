d8c9f2fffd2697b1f3b70e4a198fbc67fc6ccf3f
Mohammad Shifat E Rabbi <51829986+mr2kzUVa@users.noreply.github.com>
2021-11-24 14:56:11 -0500

    Update script.sh

--- code/script.sh
+++ code/script.sh
@@ -3,17 +3,11 @@
 # Use different models to do classification on different datasets
 
 
+python CNNc.py --dataset MNIST --model resnet18 --naug 50
 python CNNc.py --dataset MNIST --model vgg11 --naug 50
-python CNNc.py --dataset SYNTH --model vgg11 --naug 50
-python CNNc.py --dataset AFFNISTb --model vgg11 --naug 50
-python CNNc.py --dataset OMNIGLOT --model vgg11 --naug 50
-python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 50
-
-python CNNc.py --dataset AFFNISTb_out --model vgg11 --naug 25
-python CNNc.py --dataset SYNTH --model vgg11 --naug 25
-python CNNc.py --dataset OMNIGLOT --model vgg11 --naug 25
-python CNNc.py --dataset MNIST --model vgg11 --naug 25
-python CNNc.py --dataset AFFNISTb --model vgg11 --naug 25
-
+python CNNc.py --dataset MNIST --model shallowcnn --naug 50
+python TRADc.py --dataset MNIST --feature raw --classifier KNN --naug 50
+python RCDTNS.py --dataset MNIST
+python RCDTNS_0.py --dataset MNIST
 
 
